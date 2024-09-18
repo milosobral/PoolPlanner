8dd4644889606b3832a861b195ab4a8c1c0f14db
Milo Sobral <milosobral@gmail.com>
2024-09-18 14:05:25 -0400

    Added scraping logic to get all pools using Colly

--- backend/main.go
+++ backend/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"log"
 )
 
 func main() {
-	html_from_page, err := getHTML("https://montreal.ca/lieux/piscine-schubert")
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	fmt.Println(html_from_page)
+	// Get the list of pools
+	pools := getPoolList("https://montreal.ca/lieux?mtl_content.lieux.installation.code=PISI&mtl_content.lieux.available_activities.code=ACT0")
+
+	// Print the list of pools
+	for _, pool := range pools {
+		fmt.Println(pool)
+	}
 }
--- backend/scraping.go
+++ backend/scraping.go
@@ -3,25 +3,54 @@
 package main
 
 import (
-	"io"
-	"net/http"
+	"strings"
+
+	"github.com/gocolly/colly"
 )
 
-// Function to make a request to the website and get the data
-func getHTML(url string) (string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	// Read the HTML content as a byte slice
-	htmlBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
+// Pool struct
+type pool struct {
+	name          string
+	href          string
+	address       string
+	neighbourhood string
+}
 
-	// Convert the byte slice to a string
-	htmlCode := string(htmlBytes)
+// Stringer function for the pool struct
+func (p pool) String() string {
+	// Make a string with the pool name and href
+	return "Name: " + p.name + "\nURL: " + p.href + "\nAddress: " + p.address + "\nNeighbourhood: " + p.neighbourhood
+}
 
-	return htmlCode, nil
+// Scraping the pool list with Colly
+func getPoolList(url string) []pool {
+
+	pools := make([]pool, 0)
+
+	// Create a new instance of the Collector
+	c := colly.NewCollector()
+
+	c.OnHTML("a.list-group-item-action", func(e *colly.HTMLElement) {
+
+		// Get the name of the pool with the text for only the specific div
+		name := e.ChildText("div.list-group-item-title")
+
+		// Get the URL of the pool
+		href := e.Attr("href")
+
+		// Get the address of the pool
+		address := e.ChildText("div.list-group-item-infos.rm-last-child-mb")
+
+		// Get the neighbourhood of the pool
+		neighbourhood := strings.Replace(e.ChildText("div.list-group-item-infos"), address, "", 1)
+
+		// Add the pool to the list
+		pools = append(pools, pool{name, href, address, neighbourhood})
+	})
+
+	// Start scraping
+	c.Visit(url)
+
+	return pools
+
 }
